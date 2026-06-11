6ae466ab1d13c1241e5a402044576c9692a6b178
DIR\veronica.cocco <veronica.cocco@accenture.com>
2026-06-11 14:31:13 +0200

    feat(catalog): added create and filter CRUD for catalog services

--- src/main/java/com/accenture/ra/controller/CatalogServicesController.java
+++ src/main/java/com/accenture/ra/controller/CatalogServicesController.java
@@ -1,24 +1,34 @@
 package com.accenture.ra.controller;
 
+import com.accenture.ra.response.CatalogServiceResponse;
 import com.accenture.ra.response.CatalogServicesListResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//import io.swagger.v3.oas.annotations.tags.Tag;
-
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping(value="/catalog")
-//@Tag(name="Catalog", description = "Gruppo relativo agli elementi a catalogo")
+@RequestMapping(value="/catalog/services")
+@Tag(name="Catalog", description = "Gruppo relativo agli elementi a catalogo")
 public class CatalogServicesController {
 
-	@GetMapping(value = "/services")
+	@GetMapping(value = "")
 	public ResponseEntity<CatalogServicesListResponse> getCatalogServicesList() {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
+	@PostMapping(value = "")
+	public ResponseEntity<CatalogServiceResponse> createCatalogService() {
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/filter")
+	public ResponseEntity<CatalogServicesListResponse> getFilteredServices() {
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 }
---
+++ src/main/java/com/accenture/ra/response/CatalogServiceResponse.java
@@ -0,0 +1,5 @@
+package com.accenture.ra.response;
+
+public class CatalogServiceResponse {
+
+}
