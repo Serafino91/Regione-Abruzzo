19e00d0760920af3f0293b6accb8a1a558b64675
DIR\veronica.cocco <veronica.cocco@accenture.com>
2026-06-17 14:53:56 +0200

    feat(entities): added entity class for service_type table

---
+++ src/main/java/com/accenture/ra/entity/ServiceTypeEntity.java
@@ -0,0 +1,29 @@
+package com.accenture.ra.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "service_type")
+public class ServiceTypeEntity {
+
+    @Id
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+  
+}
