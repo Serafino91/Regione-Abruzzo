635fe1c21d8c04380d3b2d20309ef6cc44220b95
DIR\veronica.cocco <veronica.cocco@accenture.com>
2026-06-17 15:18:27 +0200

    feat(entities): edited/addededited/added entities for service and param tables

---
+++ src/main/java/com/accenture/ra/entity/ParamEntity.java
@@ -0,0 +1,41 @@
+package com.accenture.ra.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "param")
+public class ParamEntity {
+
+	@Id
+    private String id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "param_type")
+	private String paramType;
+	
+	@Column(name = "min_value")
+	private String minValue;
+	
+	@Column(name = "max_value")
+	private String maxValue;
+	
+	@Column(name = "min_value")
+	private boolean isRequired;
+	
+	@Column(name = "service_id")
+	private String serviceId;
+	
+}
--- src/main/java/com/accenture/ra/entity/ServiceEntity.java
+++ src/main/java/com/accenture/ra/entity/ServiceEntity.java
@@ -20,36 +20,15 @@ public class ServiceEntity {
     @Id
     private String id;
 
-//    @Column(name = "catalog_id")
-//    private String catalogId;
-
-    @Column(name = "type")
-    private String serviceType;
-
-    @Column(name = "item")
-    private String itemName;
+    @Column(name = "name")
+    private String name;
 
     @Column(name = "base")
     private Boolean isBase;
 
     @Column(name = "optional")
     private Boolean isOptional;
-
-    @Column(name = "vcpu")
-    private Integer vcpu;
-
-    @Column(name = "vram_gb")
-    private Integer vramGb;
-
-    @Column(name = "storage_gb")
-    private Integer storageGb;
-
-    @Column(name = "minimum_technical_features")
-    private String minimumTechnicalFeatures;
-
-    @Column(name = "quantity")
-    private Integer quantity;
-
-    @Column(name = "duration_months")
-    private Integer durationMonths;
+    
+    @Column(name = "type")
+    private ServiceTypeEntity serviceType;
 }
