19292ec53f4a5c6b0140ccd13f5a4d515a18d903
Serafino Conforti <s.conforti29@gmail.com>
2026-06-25 10:01:30 +0200

    Added compoment richieste-table

--- FE-Regione-Abruzzo/abruzzo/src/app/pages/richieste/richieste.ts
+++ FE-Regione-Abruzzo/abruzzo/src/app/pages/richieste/richieste.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, DestroyRef, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RichiesteTable } from '../../components/richieste-table/richieste-table';
 import { RichiestaModel } from '../../model/richiestaModel';
@@ -9,29 +9,32 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-richieste',
-  imports: [RouterLink],
-  // imports: [RouterLgink, RichiesteTable],
+  // imports: [RouterLink],
+  imports: [RouterLink, RichiesteTable],
   templateUrl: './richieste.html',
   styleUrl: './richieste.css',
 })
 export class Richieste implements OnInit {
   private destroyRef = inject(DestroyRef);
+  private platformId = inject(PLATFORM_ID);
+  private cdr = inject(ChangeDetectorRef);
   constructor(private richiestaService: RichiesteService) {}
 
   ngOnInit(): void {
     this.getRichieste();
   }
 
-
   listaRichieste: RichiestaModel[] = [];
 
   private getRichieste() {
-    this.richiestaService.getAllRichieste()
+    this.richiestaService
+      .getAllRichieste()
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         next: (resp) => {
           this.listaRichieste = resp;
-          console.log("resp richieste: ",this.listaRichieste);
+          console.log('resp richieste: ', this.listaRichieste);
+          this.cdr.detectChanges();
         },
         error: (err) => {
           console.error('Errore nel recupero richieste:', err);
