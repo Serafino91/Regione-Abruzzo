e8f71691a96d9688219228ed4eb94b66280f84ad
gianluca.dagostino <gianluca.dagostino@accenture.com>
2026-07-07 15:15:51 +0200

    [Back-end] fix RaAuthController.

--- BE-Regione-Abruzzo/src/main/java/com/accenture/ra/controller/RaAuthController.java
+++ BE-Regione-Abruzzo/src/main/java/com/accenture/ra/controller/RaAuthController.java
@@ -1,5 +1,7 @@
 package com.accenture.ra.controller;
 
+import com.accenture.ra.dto.response.RaAuthUserResponse;
+import com.accenture.ra.service.AdfsTokenService;
 import com.accenture.ra.service.RaAuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,6 +15,8 @@ import java.util.Map;
 @RequestMapping("/api/v1/ra")
 public class RaAuthController {
 
+    private AdfsTokenService adfsTokenService;
+
     private final RaAuthService raAuthService;
 
     public RaAuthController(RaAuthService raAuthService) {
@@ -20,7 +24,7 @@ public class RaAuthController {
     }
 
     @GetMapping("/verifica-utente")
-    public ResponseEntity<Map<String, Object>> verificaOperatoreInCascata(@AuthenticationPrincipal OidcUser principal) {
+    public ResponseEntity<RaAuthUserResponse> verificaOperatoreInCascata(@AuthenticationPrincipal OidcUser principal) {
         if (principal == null) {
             return ResponseEntity.status(401).build();
         }
@@ -30,7 +34,13 @@ public class RaAuthController {
         String cleanFiscalNumber = (rawFiscalNumber != null) ? rawFiscalNumber.replace("TINIT-", "") : "RSSMRA80A01H501U"; // Fallback se il mock SPID è vuoto
 
         // 2. Passiamo il CF al servizio che recupererà il token ADFS e interrogherà la Regione
-        Map<String, Object> esitoRegione = raAuthService.verificaPosizione(cleanFiscalNumber);
+
+        RaAuthUserResponse esitoRegione =
+                raAuthService.searchUser(
+                        cleanFiscalNumber,
+                        adfsTokenService.getAdfsToken()
+                );
+
 
         return ResponseEntity.ok(esitoRegione);
     }
