ce2f57fcf95a65e4adb9d8fa51fde6ae815a16c4
Marco Napoletano <m.napoletano@accenture.com>
2026-08-03 17:50:31 +0200

    FE: implementate bozze delle rispettive parti in dettaglio di "Incident Ticket" ed "Accreditamento ticket".

--- FE-Regione-Abruzzo/abruzzo/src/app/pages/dettaglio-accreditamento-ticket/dettaglio-accreditamento-ticket.ts
+++ FE-Regione-Abruzzo/abruzzo/src/app/pages/dettaglio-accreditamento-ticket/dettaglio-accreditamento-ticket.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { PageHeader } from '../../components/page-header/page-header';
+import { InfoBar } from "../../components/info-bar/info-bar";
 
 @Component({
-  selector: 'app-dettaglio-accreditamento-ticket',
-  imports: [],
-  templateUrl: './dettaglio-accreditamento-ticket.html',
-  styleUrl: './dettaglio-accreditamento-ticket.css',
+    selector: 'app-dettaglio-accreditamento-ticket',
+    imports: [
+        PageHeader,
+        InfoBar
+    ],
+    templateUrl: './dettaglio-accreditamento-ticket.html',
+    styleUrl: './dettaglio-accreditamento-ticket.css',
 })
-export class DettaglioAccreditamentoTicket {}
+
+export class DettaglioAccreditamentoTicket {
+
+    infoIncidentTicket = [
+        {
+            label: 'Nome',
+            value: "Mario",
+            icon: 'it-file',
+        },
+        {
+            label: 'Cognome',
+            value: "Rossi",
+            icon: 'it-arrow-up',
+        },
+        {
+            label: 'Data richiesta',
+            value: "31/07/2026",
+            icon: 'it-calendar',
+        },
+    ];
+
+    showDeleteModal = false;
+
+}
