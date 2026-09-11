61115a556da04514bbec363a7b737a9f344ab851
Marco Napoletano <m.napoletano@accenture.com>
2026-09-11 10:53:09 +0200

    [FRONT-END]: fix route dettaglio in "Tabella incident ticket"; Sostituite dropdown con pulsanti sulle tabelle "Files; aggiunto spinner su API getTickets() nella sezione principale incident.

--- FE-Regione-Abruzzo/abruzzo/src/app/pages/incident/incident.ts
+++ FE-Regione-Abruzzo/abruzzo/src/app/pages/incident/incident.ts
@@ -5,16 +5,18 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { IncidentService } from '../../services/incident.service';
 import { TicketModel } from '../../model/ticket.model';
 import { TabellaIncident } from '../../sections/incident/tabella-incident/tabella-incident';
+import { SpinnerCard } from '../../components/spinner-card/spinner-card';
 
 @Component({
   selector: 'app-incident',
-  imports: [FiltriIncident, PageHeader, TabellaIncident],
+  imports: [FiltriIncident, PageHeader, TabellaIncident, SpinnerCard],
   standalone: true,
   templateUrl: './incident.html',
   styleUrl: './incident.css',
 })
 export class Incident implements OnInit {
   tickets: TicketModel[] = [];
+  isLoading = false;
   private destroyRef = inject(DestroyRef);
   private cdr = inject(ChangeDetectorRef);
 
@@ -24,6 +26,8 @@ export class Incident implements OnInit {
     this.getAllTickets();
   }
   private getAllTickets(): void {
+    this.isLoading = true;
+
     this.incident
       .getTickets()
       .pipe(takeUntilDestroyed(this.destroyRef))
@@ -32,10 +36,12 @@ export class Incident implements OnInit {
           console.log(resp);
           this.tickets = resp;
           console.log('resp tickets: ', this.tickets);
+          this.isLoading = false;
           this.cdr.detectChanges();
         },
         error: (err) => {
           console.error('Errore nel recupero richieste:', err);
+          this.isLoading = false;
         },
       });
   }
